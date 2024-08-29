c964e80c818dab23b447d8c0fb703fcd7bc03063
Willian Medeiros <willianmedeiroslm@gmail.com>
2024-08-29 10:16:41 -0300

    feature: create base project

---
+++ Data/FilmeContext.cs
@@ -0,0 +1,14 @@
+using FilmeApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmeApi.Data;
+
+public class FilmeContext : DbContext
+{
+    public FilmeContext(DbContextOptions<FilmeContext> opts) : base(opts)
+    {
+
+    }
+
+    public DbSet<Filme> Filmes { get; set; }
+}
---
+++ controllers/FilmeController.cs
@@ -0,0 +1,46 @@
+using AutoMapper;
+using FilmeApi.Data;
+using FilmeApi.Data.Dtos;
+using FilmeApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmeApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    private FilmeContext _context;
+    private IMapper _mapper;
+
+    public FilmeController(FilmeContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+
+    [HttpPost]
+    public IActionResult AdicionarFilme([FromBody] CreateFilmeDto filmeDto)
+    {
+        Filme filme = _mapper.Map<Filme>(filmeDto);
+        _context.Filmes.Add(filme);
+        _context.SaveChanges();
+
+        return CreatedAtAction(nameof(RecuperaFilmePorId), new { id = filme.Id }, filme);
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip = 0, [FromQuery] int take = 10)
+    {
+        return _context.Filmes.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult RecuperaFilmePorId(int id)
+    {
+        var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+        if (filme == null) return NotFound();
+        return Ok(filme);
+    }
+}
