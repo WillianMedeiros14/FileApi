51e1475d95b2a17b88489798c05442ddcad9bc6f
Willian Medeiros <willianmedeiroslm@gmail.com>
2024-08-29 10:27:21 -0400

    feature: Updating movie by id with PATCH

--- Program.cs
+++ Program.cs
@@ -11,6 +11,10 @@ builder.Services.AddDbContext<FilmeContext>(opts =>
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 // Add services to the container.
+builder.Services.AddControllers().AddNewtonsoftJson();
+
+//código omitido
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
