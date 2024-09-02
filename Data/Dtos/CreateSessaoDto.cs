357a1841cacb49dc8506061973fd9dcaf0edd6d6
Willian Medeiros <willianmedeiroslm@gmail.com>
2024-09-02 11:56:49 -0400

    add sessao

---
+++ Data/Dtos/CreateSessaoDto.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmeApi.Data.Dtos
+{
+    public class CreateSessaoDto
+    {
+    }
+}
---
+++ Data/Dtos/ReadSessaoDto.cs
@@ -0,0 +1,7 @@
+namespace FilmeApi.Data.Dtos
+{
+    public class ReadSessaoDto
+    {
+        public int Id { get; set; }
+    }
+}
